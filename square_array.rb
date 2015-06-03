ddb5af06c1447d56626165e0f7231e80d759e92f
AnnaErshova <anna.ershova@gmail.com>
2015-06-03 16:10:26 -0400

    all done

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  new_array = []
+  array.each do |x|
+    new_array << x*x
+  end
+  new_array
 end
+
+# This method creates a new array and then pushes a squared array into it. 
+# There should be a way to do it differently.
